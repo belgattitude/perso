4fc016acb6a71c835b263428f7109a18c22ed9e2
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-02 19:01:38 +0200

    feat: some more tests

--- apps/website/src/features/meet/pages/MeetPage.tsx
+++ apps/website/src/features/meet/pages/MeetPage.tsx
@@ -1,26 +1,14 @@
-import { css } from '@emotion/react';
 import { NextSeo } from 'next-seo';
 import dynamic from 'next/dynamic';
 import type { FC } from 'react';
-import create from 'zustand';
-import type { JitsiEmbedProps } from '../components/jitsi';
-
-interface UiState {
-  videoEmbedStatus: 'open' | 'closed';
-  openVideoEmbed: () => void;
-  closeVideoEmbed: () => void;
-}
-
-const useStore = create<UiState>()((set) => ({
-  videoEmbedStatus: 'closed',
-  openVideoEmbed: () => set((state) => ({ videoEmbedStatus: 'open' })),
-  closeVideoEmbed: () => set((state) => ({ videoEmbedStatus: 'closed' })),
-}));
+import { Header } from '@/features/meet/components/Header';
+import type { JitsiEmbedProps } from '../components/Jitsi';
+import { useStore } from '../stores';
 
 const JitsiEmbed = dynamic<JitsiEmbedProps>(
   () =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    import('../components/jitsi').then(({ JitsiEmbed }) => JitsiEmbed) as any,
+    import('../components/Jitsi').then(({ JitsiEmbed }) => JitsiEmbed) as any,
   {
     ssr: false,
   }
@@ -65,7 +53,7 @@ export const Welcome: FC<Props> = (props) => {
       </button>
       <button
         type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        className="py-2.5 px-5 mr-2 mb-2 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
       >
         Not you ?
       </button>
@@ -79,11 +67,8 @@ export const MeetPage: FC<Props> = (props) => {
   return (
     <>
       <NextSeo nofollow={true} noindex={true} />
-      <div
-        css={css`
-          margin: 50px;
-        `}
-      >
+      <Header />
+      <div className="container mx-auto px-4">
         {videoEmbedStatus === 'open' ? (
           <Jitsi {...props} />
         ) : (
