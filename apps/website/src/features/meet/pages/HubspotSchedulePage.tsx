737ca177c72b525a9aa5b80b18cb7e933a5b4814
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-05-24 11:48:29 +0200

    feat: passing params

--- apps/website/src/features/meet/pages/HubspotSchedulePage.tsx
+++ apps/website/src/features/meet/pages/HubspotSchedulePage.tsx
@@ -8,7 +8,19 @@ type Props = {
 export const HubspotSchedulePage: FC<Props> = (props) => {
   const { user } = props;
 
-  const link = `https://meetings.hubspot.com/${user}?embed=true`;
+  const prefillData = {
+    email: 'belgattitude@gmail.com',
+    firstName: 'Sébastien',
+    lastName: 'Vanvelthem',
+    phoneNumber: '+32476421608',
+  };
+
+  const params = new URLSearchParams({
+    embed: 'true',
+    ...prefillData,
+  });
+
+  const link = `https://meetings.hubspot.com/${user}?${params}`;
   return (
     <>
       <Script
