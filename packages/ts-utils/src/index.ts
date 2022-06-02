c31d22cd0ea67033abf15e2b6a36ea9324344256
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-06-02 22:01:07 +0200

    feat: change something

--- packages/ts-utils/src/index.ts
+++ packages/ts-utils/src/index.ts
@@ -1,6 +1,5 @@
 export { Asserts } from './utils/asserts';
 export { ArrayUtils } from './utils/array-utils';
-export { RandomUtils } from './utils/random-utils';
-
+export * from './random';
 export * from './utils/typeguards';
 export type { UnPromisify } from './utils/type-utils';
