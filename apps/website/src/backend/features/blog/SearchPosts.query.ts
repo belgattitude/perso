a92426223cad35c2c55ccc6834ddc747b85398b7
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-06-08 12:56:39 +0200

    feat: posts in database

---
+++ apps/website/src/backend/features/blog/SearchPosts.query.ts
@@ -0,0 +1,28 @@
+import type { PrismaClientDbMain } from '@belgattitude/db-main';
+// import { PrismaDbMain } from '@belgattitude/db-main';
+import { AbstractCacheableSearchQuery } from '@/backend/lib/query';
+import type { CacheKeyGenParams } from '@/backend/lib/query/ICacheableSearchQuery';
+
+type SearchParams = {
+  ftQuery?: string;
+};
+
+export class SearchPostsQuery extends AbstractCacheableSearchQuery<SearchParams> {
+  readonly queryName = 'SearchPostsQuery';
+  cacheParams: CacheKeyGenParams = {
+    version: '1.0.0',
+  };
+
+  constructor(private readonly prisma: PrismaClientDbMain) {
+    super();
+  }
+
+  execute = async (params: SearchParams) => {
+    const { ftQuery: _ftQuery } = params;
+    return this.prisma.post.findMany({
+      where: {
+        deletedAt: null,
+      },
+    });
+  };
+}
