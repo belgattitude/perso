dabddcf73b4104157692b6516e0ec5088f136edb
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-06-11 12:51:56 +0200

    chore: improve tsup builds

--- packages/crypto/tsup.config.js
+++ packages/crypto/tsup.config.js
@@ -6,8 +6,11 @@ export default defineConfig((options) => {
     splitting: true,
     clean: true,
     dts: true,
-    tsconfig: './tsconfig.build.json',
+    format: ['esm'],
+    platform: 'node',
+    target: ['node14'],
+    tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
     sourcemap: !options.watch,
-    minify: !options.watch,
+    minify: false, // do not minify for a node package, so it's possible to patch the code (yarn patch / patch-package)
   };
 });
--- packages/ts-utils/tsup.config.js
+++ packages/ts-utils/tsup.config.js
@@ -6,7 +6,10 @@ export default defineConfig((options) => {
     splitting: true,
     clean: true,
     dts: true,
-    tsconfig: './tsconfig.build.json',
+    format: ['esm'],
+    platform: 'browser',
+    target: ['es2017', 'chrome70', 'edge18', 'firefox70', 'node14'],
+    tsconfig: new URL('./tsconfig.build.json', import.meta.url).pathname,
     sourcemap: !options.watch,
     minify: !options.watch,
   };
