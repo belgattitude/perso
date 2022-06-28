d7ed124c5a629c1572c62d577599f04964dbacaf
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-06-28 12:05:47 +0200

    feat: wip for NEXT_PUBLIC_VERCEL_URL

--- apps/website/src/config/trpc-config.ts
+++ apps/website/src/config/trpc-config.ts
@@ -2,16 +2,23 @@ import type { withTRPC } from '@trpc/next';
 
 export type WithTrpcConfig = Parameters<typeof withTRPC>[0];
 
+const isServer = () => {
+  return typeof window === 'undefined';
+};
+
 export const getWithTrpcConfig = (): WithTrpcConfig => {
-  let url = '';
-  if (process.env.NODE_ENV === 'development') {
-    url = 'http://localhost:3000/api/trpc';
-  } else if (process.env.VERCEL_URL) {
-    url = `https://${process.env.VERCEL_URL}/api/trpc`;
-  } else if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-    url = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`;
-  } else {
-    throw new Error('Cannot get vercel url');
+  const vercelUrl =
+    process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL;
+  const localDevUrl =
+    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : false;
+
+  let url = '/api/trpc';
+  if (isServer()) {
+    if (localDevUrl) {
+      url = `${localDevUrl}${url}`;
+    } else {
+      url = `${vercelUrl}/${url}`;
+    }
   }
 
   return {
