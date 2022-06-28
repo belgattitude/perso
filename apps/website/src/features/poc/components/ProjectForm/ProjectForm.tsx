c6f4f180a197dafd20901fb024f92b9e01f1b89c
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-06-28 11:24:47 +0200

    feat: attempt to log what could be wrong

--- apps/website/src/features/poc/components/ProjectForm/ProjectForm.tsx
+++ apps/website/src/features/poc/components/ProjectForm/ProjectForm.tsx
@@ -17,12 +17,13 @@ const schema = getProjectFormSchema();
 export const ProjectForm: FC = (props) => {
   const router = useRouter();
   const mutation = trpc.useMutation(['poc/create_project'], {
-    ssr: false,
     onSettled: (data) => {
+      console.log('poc request resolved');
       const { slug } = data ?? {};
       if (!isNonEmptyString(slug)) {
         console.error("Error: can't acquire the project slug");
       }
+      console.log('Going to new route', `/poc/sortlist/project/${slug}`);
       router.push(`/poc/sortlist/project/${slug}`);
     },
   });
