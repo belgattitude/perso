de3dcd28697da6abac86f94e6876e8a7233aa582
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-07-02 21:56:24 +0200

    feat: simplify configs

--- apps/website/src/features/about/about.config.ts
+++ apps/website/src/features/about/about.config.ts
@@ -1,13 +1,10 @@
 import type { I18nActiveNamespaces } from '@/lib/i18n';
 
 export type AboutConfig = {
-  // Define installed namespaces in the type here
-  // to allow full typechecking of your translation keys.
-  i18nNamespaces: Readonly<
-    I18nActiveNamespaces<'common' | 'about' | 'navigation'>
-  >;
+  // Define namespaces in use in both the type and the config.
+  i18nNamespaces: I18nActiveNamespaces<'common' | 'about' | 'navigation'>;
 };
 export const aboutConfig: AboutConfig = {
   /** Namespaces that should be loaded for this page */
   i18nNamespaces: ['common', 'about', 'navigation'],
-} as const;
+};
--- apps/website/src/features/blog/blog.config.ts
+++ apps/website/src/features/blog/blog.config.ts
@@ -1,13 +1,10 @@
 import type { I18nActiveNamespaces } from '@/lib/i18n';
 
 export type BlogConfig = {
-  // Define installed namespaces in the type here
-  // to allow full typechecking of your translation keys.
-  i18nNamespaces: Readonly<
-    I18nActiveNamespaces<'common' | 'blog' | 'navigation'>
-  >;
+  // Define namespaces in use in both the type and the config.
+  i18nNamespaces: I18nActiveNamespaces<'common' | 'blog' | 'navigation'>;
 };
 
 export const blogConfig: BlogConfig = {
   i18nNamespaces: ['common', 'blog', 'navigation'],
-} as const;
+};
