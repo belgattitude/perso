92e2fdcab3ba44b9f952fa7fef0e893cede9ce41
Sébastien Vanvelthem <belgattitude@gmail.com>
2022-07-19 17:57:28 +0200

    feat: add name

--- packages/crypto/src/encrypt/exception/WeakPasswordError.ts
+++ packages/crypto/src/encrypt/exception/WeakPasswordError.ts
@@ -2,5 +2,6 @@ export class WeakPasswordError extends Error {
   constructor(message: string) {
     super(message);
     Object.setPrototypeOf(this, WeakPasswordError.prototype);
+    this.name = WeakPasswordError.prototype.constructor.name;
   }
 }
